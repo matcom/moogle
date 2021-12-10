075c8cdd80ac34c698d1496dc150e5bece98d052
Alejandro Piad <apiad@apiad.net>
2021-12-10 15:35:10 -0500

    Actualización con score

---
+++ MoogleEngine/SearchItem.cs
@@ -0,0 +1,17 @@
+namespace MoogleEngine;
+
+public class SearchItem
+{
+    public SearchItem(string title, string snippet, float score)
+    {
+        this.Title = title;
+        this.Snippet = snippet;
+        this.Score = score;
+    }
+
+    public string Title { get; private set; }
+
+    public string Snippet { get; private set; }
+
+    public float Score { get; private set; }
+}
