aab17b5db54d882017ca5c01bbd222919b174a8b
sardlimad <genesis.david.08@gmail.com>
2021-12-18 00:30:19 -0600

    Busca Archivos por nombre exacto

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -3,15 +3,55 @@
 
 public static class Moogle
 {
-    public static SearchResult Query(string query) {
-        // Modifique este método para responder a la búsqueda
+    public static SearchResult Query(string query)
+    {
+        SearchItem[] Elemento= new SearchItem[ReadFolder().Length];
+        string[] read = ReadFolder();
+        for (int i = 0; i < ReadFolder().Length; i++)
+        {
+            if (read[i]==query)
+            {
+                Elemento[i]= new SearchItem(read[i], "snippet", 0.2f);
+            }
+            
+        }
 
-        SearchItem[] items = new SearchItem[3] {
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
-            new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.1f),
-        };
-
-        return new SearchResult(items, query);
+        return new SearchResult(Elemento, query);
     }
+    
+    //Método para leer la carpeta y ver los archivos que contiene.
+    public static string[] ReadFolder()
+    {
+
+        DirectoryInfo dir = new DirectoryInfo(Directory.GetCurrentDirectory() + @"/Carpeta archivos");
+        FileInfo[] files = dir.GetFiles("*.txt");
+
+        string[] Archivo = new string[files.Length];
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            
+            Archivo[i]=files[i].Name;
+            
+        }
+
+        // string [] files = DirectoryInfo.GetFiles(Directory.GetCurrentDirectory()+@"/Carpeta archivos","*.txt");
+        // foreach (var file in files)
+        // {
+        //     Console.WriteLine(file);
+        // }
+        // SearchItem[] Archivo = new SearchItem[files.Length];
+
+        // for (int i = 0; i < files.Length; i++)
+        // {
+        //     Archivo[i] = new SearchItem(Path.GetFileName(files[i]),"snippet",(float)i);
+        // }
+
+        return Archivo;
+    }
+
+    //Método para leer los archivos de la carpeta.
+    
+
+
 }
